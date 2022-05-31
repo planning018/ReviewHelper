2992162b1f7e1bfd9191a9575c82f63dd315fa0a
planning018 <13057588636@163.com>
2022-05-31 14:34:05 +0800

    update setting

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const BASE_URL = "https://api.notion.com/";
 // query settings
 // need to think about how to adjust it 
 const QUERY_NUM = 2;
-const QUERY_KEYS = ['Name', 'Intro', 'Created'];
+const QUERY_KEYS = ['Name', 'Tags', 'Desc', 'CreateTime', 'URL'];
 
 (async () => {
   // Query
